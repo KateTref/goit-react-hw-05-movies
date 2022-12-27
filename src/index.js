1bb91cd3c64841afad639c3258fb17a35d8a2042
KateTref <trefilovaekaterina408@gmail.com>
2022-12-27 02:11:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="goit-react-hw-05-movies">
       <App />
-      <ToastContainer />
+      {/* <ToastContainer /> */}
     </BrowserRouter>
   </React.StrictMode>
 );
